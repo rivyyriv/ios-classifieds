17e1fc8c7a11c62e17cab89a769d22fd6cb18fdf
River Smith <rivyyriv@gmail.com>
2023-11-09 17:01:35 -0700

    Initial Commit

---
+++ classifieds/classifiedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  classifiedsApp.swift
+//  classifieds
+//
+//  Created by River Smith on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct classifiedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
